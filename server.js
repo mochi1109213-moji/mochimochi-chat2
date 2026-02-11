7e6bb447a179763efdc1ee8a6106ed30e966e079
mochimochi-1109 <soma1109213@gmail.com>
2026-02-11 18:57:29 +0900

    server.js

--- server.js
+++ server.js
@@ -11,21 +11,26 @@ const client = new Client({
 
 client.connect();
 
-// テーブルをアップデート（nameとtimeを追加）
+// テーブル作成（名前、内容、時間を保存できるようにする）
 client.query('CREATE TABLE IF NOT EXISTS messages (id SERIAL PRIMARY KEY, name TEXT, content TEXT, time TEXT)');
 
+// 【重要】HTMLファイルが入っているフォルダを指定する
+app.use(express.static('public'));
+
 io.on('connection', async (socket) => {
-  // 過去ログ取得
+  // 過去のメッセージをDBから読み込んで表示する
   const res = await client.query('SELECT name, content, time FROM messages ORDER BY id ASC');
   res.rows.forEach(row => {
     socket.emit('chat message', { name: row.name, message: row.content, time: row.time });
   });
 
   socket.on('chat message', async (data) => {
-    // DB保存
+    // 送信されたメッセージをDBに保存する
     await client.query('INSERT INTO messages (name, content, time) VALUES ($1, $2, $3)', [data.name, data.message, data.time]);
+    // 全員にメッセージを送る
     io.emit('chat message', data);
   });
 });
 
+// ポート設定
 http.listen(process.env.PORT || 3000);
